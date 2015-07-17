56bdb00b49395149fe15ba687f0c41f2d213c044
John MacFarlane <jgm@berkeley.edu>
2015-07-17 14:08:16 -0700

    Improvements.

--- pdf.c
+++ pdf.c
@@ -8,6 +8,8 @@
 #include <setjmp.h>
 #include "hpdf.h"
 
+#define LINE_SPREAD 1.2
+
 jmp_buf env;
 
 #ifdef HPDF_DLL
@@ -27,6 +29,7 @@ error_handler (HPDF_STATUS   error_no,
 struct render_state {
 	HPDF_Doc pdf;
 	HPDF_Font font;
+	HPDF_REAL font_size;
 	HPDF_Page page;
 	float x;
 	float y;
@@ -36,30 +39,22 @@ static int
 S_render_node(cmark_node *node, cmark_event_type ev_type,
               struct render_state *state, int options)
 {
-	float tw;
-	char *page_title;
 	const char *text;
 	int entering = ev_type == CMARK_EVENT_ENTER;
 
 	switch (cmark_node_get_type(node)) {
 	case CMARK_NODE_DOCUMENT:
 		if (entering) {
-			page_title = "Hello, world";
 			/* create default-font */
 			state->font = HPDF_GetFont (state->pdf, "Helvetica", NULL);
+			state->font_size = 14;
 
 			/* add a new page object. */
 			state->page = HPDF_AddPage (state->pdf);
+			HPDF_Page_SetFontAndSize (state->page, state->font, state->font_size);
 
-			/* print the title of the page (with positioning center). */
-			HPDF_Page_SetFontAndSize (state->page, state->font, 24);
-			tw = HPDF_Page_TextWidth (state->page, page_title);
-			HPDF_Page_BeginText (state->page);
-			HPDF_Page_TextOut (state->page, (HPDF_Page_GetWidth(state->page) - tw) / 2,
-					   HPDF_Page_GetHeight (state->page) - 50, page_title);
-			HPDF_Page_EndText (state->page);
 			state->x = 50;
-			state->y = HPDF_Page_GetHeight(state->page) - 50;
+			state->y = HPDF_Page_GetHeight(state->page) - state->font_size;
 		}
 
 		break;
@@ -69,15 +64,16 @@ S_render_node(cmark_node *node, cmark_event_type ev_type,
 		if (state->y < 100) {
 			/* add a new page object. */
 			state->page = HPDF_AddPage (state->pdf);
-			HPDF_Page_SetFontAndSize (state->page, state->font, 24);
-			state->y = HPDF_Page_GetHeight(state->page) - 50;
+			HPDF_Page_SetFontAndSize (state->page, state->font, state->font_size);
+			state->y = HPDF_Page_GetHeight(state->page) - state->font_size;
 		}
 		text = cmark_node_get_literal(node);
-		tw = HPDF_Page_TextWidth (state->page, text);
 		HPDF_Page_BeginText (state->page);
 		HPDF_Page_TextOut (state->page, state->x, state->y, text);
 		HPDF_Page_EndText (state->page);
-		state->y -= 50;
+		state->x = 50;
+		state->y -= (state->font_size * LINE_SPREAD);
+		break;
 
 	default:
 		break;
@@ -91,10 +87,16 @@ int cmark_render_pdf(cmark_node *root, int options, char *outfile)
 	struct render_state state = { };
 
 	state.pdf = HPDF_New (error_handler, NULL);
+
 	if (!state.pdf) {
 		printf ("error: cannot create PdfDoc object\n");
 		return 1;
 	}
+
+	if (HPDF_UseUTFEncodings(state.pdf) != HPDF_OK) {
+		printf ("error: cannot set UTF-8 encoding\n");
+		return 1;
+	};
 
 	if (setjmp(env)) {
 		HPDF_Free(state.pdf);
